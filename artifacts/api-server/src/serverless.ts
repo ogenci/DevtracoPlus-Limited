399a080bfcd64bf1797205b343a4a23e164b43fc
ogenci <ogencidigital@gmail.com>
2026-05-25 13:04:24 +0000

    chore: remove backend and unused lib packages, simplify to pure SPA

--- artifacts/api-server/src/serverless.ts
+++
@@ -1,8 +0,0 @@
-import app from "./app.js";
-import type { Request, Response } from "express";
-
-// Export a default handler so serverless platforms (Vercel) can invoke the
-// Express app directly. Vercel will call the default export with (req, res).
-export default function handler(req: Request, res: Response) {
-  return (app as unknown as (req: Request, res: Response) => void)(req, res);
-}
